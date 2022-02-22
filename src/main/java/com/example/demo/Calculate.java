f087a573b751355d51764d68a759c2e3b6a44ac0
mevibhorgarg <364vibhor@gmail.com>
2022-02-22 14:58:46 +0530

    first pr

---
+++ src/main/java/com/example/demo/Calculate.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculate {
+
+	public Integer add(Integer a, Integer b) {
+		return a+b;
+	}
+	
+}
---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(DemoApplication.class, args);
+		Calculate calculate = new Calculate();
+		Integer res= calculate.add(1,3);
+				System.out.println(res);
+	}
+
+}
